2613a31bbe7b425e2609906eecdb0c40fc0ceaf3
Carlos Serathiuk <114114856+kuihtares@users.noreply.github.com>
2023-07-17 15:22:57 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    @NotBlank
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+}
